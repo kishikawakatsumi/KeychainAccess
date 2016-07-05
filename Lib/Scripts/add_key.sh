37a8fa6dda8b59313d5a215b2ed6784a99681686
kishikawa katsumi <kishikawakatsumi@mac.com>
2016-07-05 18:26:03 +0900

    Fix code signing issue when testing on macOS

--- Lib/Scripts/add_key.sh
+++ Lib/Scripts/add_key.sh
@@ -7,6 +7,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 security import ./Lib/Certificates/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Lib/Certificates/developer.p12 -k ~/Library/Keychains/ios-build.keychain -P $PASSPHRASE -T /usr/bin/codesign
+security import ./Lib/Certificates/mac_developer.p12 -k ~/Library/Keychains/ios-build.keychain -P $PASSPHRASE -T /usr/bin/codesign
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./Lib/Certificates/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
