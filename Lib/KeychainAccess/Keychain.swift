cb5cfce6d50316cec901520c4f2b47baa38bd150
Paul King <paul@k4ety.com>
2017-07-14 16:51:01 -0400

    Swift 4 fixes and updates

--- Lib/KeychainAccess/Keychain.swift
+++ Lib/KeychainAccess/Keychain.swift
@@ -1639,7 +1639,7 @@ extension CFError {
     var error: NSError {
         let domain = CFErrorGetDomain(self) as String
         let code = CFErrorGetCode(self)
-        let userInfo = CFErrorCopyUserInfo(self) as! [NSObject: Any]
+        let userInfo = CFErrorCopyUserInfo(self) as! [String: Any]
 
         return NSError(domain: domain, code: code, userInfo: userInfo)
     }
