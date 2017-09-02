e1568029441b81d78945fb7be6318fddd4580f51
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2017-09-02 11:02:19 +0900

    Update code signing certificates

--- Lib/Scripts/add_key.sh
+++ Lib/Scripts/add_key.sh
@@ -6,8 +6,10 @@ security unlock-keychain -p travis build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/build.keychain
 
 security import ./Lib/Certificates/apple.cer -k ~/Library/Keychains/build.keychain -T /usr/bin/codesign
-security import ./Lib/Certificates/ios_developer.p12 -k ~/Library/Keychains/build.keychain -P $PASSPHRASE -T /usr/bin/codesign
-security import ./Lib/Certificates/developer_id_app.p12 -k ~/Library/Keychains/build.keychain -P $PASSPHRASE -T /usr/bin/codesign
+security import ./Lib/Certificates/ios_developer.cer -k ~/Library/Keychains/build.keychain -T /usr/bin/codesign
+security import ./Lib/Certificates/ios_developer.p12 -k ~/Library/Keychains/build.keychain -P '' -T /usr/bin/codesign
+security import ./Lib/Certificates/developer_id_app.cer -k ~/Library/Keychains/build.keychain -T /usr/bin/codesign
+security import ./Lib/Certificates/developer_id_app.p12 -k ~/Library/Keychains/build.keychain -P '' -T /usr/bin/codesign
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./Lib/Certificates/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
