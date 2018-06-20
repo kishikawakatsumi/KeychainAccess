426a30908d4a937494e8abd16f50dce97a8723c9
Victor Hugo Barros <hey.zooi@gmail.com>
2018-06-20 12:46:48 -0700

    adding support to Swift 4 for Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -1,13 +1,26 @@
-//
-//  Package.swift
-//  KeychainAccess
-//
-//  Created by kishikawa katsumi on 2015/12/4.
-//  Copyright (c) 2015 kishikawa katsumi. All rights reserved.
-//
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "KeychainAccess"
+    name: "KeychainAccess",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KeychainAccess",
+            targets: ["KeychainAccess"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KeychainAccess",
+            dependencies: []),
+    ],
+    swiftLanguageVersions: [3, 4]
 )
