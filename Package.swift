a09556d7a385182226469827fdeb158c6aaf2c7f
Andrew Mayers <andrew@mayers.us>
2020-06-23 13:18:34 -0400

    Update deployment target to iOS 9
    With Xcode 12, iOS 8 is no longer supported as a deployment target, and it throws a warning if the package tries to support 8.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ import PackageDescription
 let package = Package(
     name: "KeychainAccess",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         .library(name: "KeychainAccess", targets: ["KeychainAccess"])
