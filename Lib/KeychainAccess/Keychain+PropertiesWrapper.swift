35e84d560a6466da3ab59e0d3091b034ea90bb59
Nghia Nguyen <quangnghiadev@gmail.com>
2020-09-12 13:30:45 +0700

    Add Properties Wrapper Extension

---
+++ Lib/KeychainAccess/Keychain+PropertiesWrapper.swift
@@ -0,0 +1,56 @@
+//
+//  Keychain+PropertiesWrapper.swift
+//
+//
+//  Created by Stuart Nguyen on 9/12/20.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct KeychainString {
+    private let keychain: Keychain
+    let key: String
+
+    init(serviceName: String, key: String) {
+        self.keychain = Keychain(service: serviceName)
+        self.key = key
+    }
+
+    public var wrappedValue: String? {
+        get {
+            return try? keychain.getString(key)
+        }
+        set {
+            if let value = newValue {
+                try? keychain.set(value, key: key)
+            } else {
+                try? keychain.remove(key)
+            }
+        }
+    }
+}
+
+@propertyWrapper
+public struct KeychainData {
+    private let keychain: Keychain
+    let key: String
+
+    init(serviceName: String, key: String) {
+        self.keychain = Keychain(service: serviceName)
+        self.key = key
+    }
+
+    public var wrappedValue: Data? {
+        get {
+            return try? keychain.getData(key)
+        }
+        set {
+            if let value = newValue {
+                try? keychain.set(value, key: key)
+            } else {
+                try? keychain.remove(key)
+            }
+        }
+    }
+}
