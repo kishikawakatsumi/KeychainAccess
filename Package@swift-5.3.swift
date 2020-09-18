654d52d30f3dd4592e944c3e0bccb53178c992f6
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2020-09-19 06:49:25 +0900

    Bump version to 4.2.1

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -18,6 +18,6 @@ let package = Package(
         .library(name: "KeychainAccess", targets: ["KeychainAccess"])
     ],
     targets: [
-        .target(name: "KeychainAccess", path: "Lib/KeychainAccess")
+        .target(name: "KeychainAccess", path: "Lib/KeychainAccess", exclude:["Info.plist"])
     ]
 )
